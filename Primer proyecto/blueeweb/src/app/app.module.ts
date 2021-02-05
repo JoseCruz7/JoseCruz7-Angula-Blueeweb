183975d46436eccd5b5c3e7ec65b2047601e3e2d
Jose Elias Cruz Perez <JC5286GT@LIDER.CORP>
2021-02-05 16:38:59 -0600

    Iniciando el curso de Angular con Blueeweb

---
+++ Primer proyecto/blueeweb/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CabeceraComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
