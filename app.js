0672a18cf75384bd0ac4d628f5c1f41ad0d26e41
Jose Elias Cruz Perez <JC5286GT@LIDER.CORP>
2021-02-10 14:04:37 -0600

    Examen de ts de Udemy

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
 "use strict";
-(() => {
-    //Las clases se Debden de iniciar con la palabra reservada class y la primera letra es mayuscula
-    class Avenger {
-        //Se necesitan establecer en el constructuor, funcion que se ejecuta cuando inicia una instancia
-        constructor(name, team, realName, figth, winFigth) {
-            this.name = name;
-            this.team = team;
-            this.realName = realName;
-            this.figth = figth;
-            this.winFigth = winFigth;
-        }
-    }
-    const antman = new Avenger();
-})();
